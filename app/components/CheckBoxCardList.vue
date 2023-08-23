e0b26de3b01adff1a656a155c4caa6054638d171
Dhéliosis <fassot.caroline@gmail.com>
2023-08-23 15:10:36 +0200

    checkbox card component

---
+++ app/components/CheckBoxCardList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Checkbox from './CheckboxCard.vue';
+
+const props = defineProps(
+	{
+		inputs: Array
+	}
+)
+
+</script>
+
+<template>
+	<ul class=" flex flex-wrap w-full gap-6">
+		<Checkbox v-for="input in inputs" :title="input.title" />
+	</ul>
+</template>
