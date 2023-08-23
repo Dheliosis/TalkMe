2d1451c13252f9bf01e1f2192e3731fa918909ac
Pierre <pierre61250@yahoo.fr>
2023-08-23 14:35:50 +0200

    feat: ajout des couleurs

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -10,7 +10,27 @@ module.exports = {
     "./node_modules/flowbite/**/*.{js,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#FFF7E5',
+          100: '#FFEECC',
+          200: '#FFDD99',
+          300: '#FFCC66',
+          400: '#FFBB33',
+          500: '#FFAA00',
+          600: '#CC8800',
+          700: '#996600',
+          800: '#664400',
+          900: '#332200',
+          950: '#1A1100',
+        },
+        font: {
+          primary: "#1C1A17",
+          secondary: "#A39C8F"
+        },
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
