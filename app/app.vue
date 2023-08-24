790336475debc00dd912681a15278aa444700ce5
Pierre <pierre61250@yahoo.fr>
2023-08-24 17:08:34 +0200

    feat

--- app/app.vue
+++ app/app.vue
@@ -105,80 +105,61 @@ const radioInputsEntrepriseSize = [
 </script>
 
 <template>
+  <Header progress1="10" progress2="100" progress3="89" :full="false"></Header>
   <div class="mx-24 my-10 ">
     <div class="flex w-full">
 
       <div class="w-1/2 h-screen scroll-smooth">
-        <section id="entrepriseType">
-          <div class="w-max mb-7">
-            <h1 class="text-2xl font-semibold">Informations de votre entreprise</h1>
-            <div class="w-1/3 h-1 bg-primary-500 rounded mt-2"></div>
-          </div>
-          <p class="w-5/6">
-            Personnalisez votre module industriel en toute simplicité avec notre formulaire dédié. Nous vous accompagnons
-            pas
-            à
-            pas pour une solution sur mesure, adaptée à vos besoins.
-          </p>
-
-          <div class="mt-7">
-            <h2 class="text-xl font-semibold">Quels type d'entreprise êtes-vous ? (secteur d'activité)</h2>
-            <div class="mt-5">
-              <RadioCardList :inputs="inputsEntrepriseType" />
-            </div>
-            <div class="h-max flex mt-2 justify-end w-11/12">
-              <a href="#entrepriseSize">
-                <Button action="Suivant" />
-              </a>
-            </div>
-          </div>
-
-        </section>
-        <section id="entrepriseSize" class="mt-24 h-screen flex items-center w-full">
-          <div class="w-full">
-            <h2 class="text-xl font-semibold">Quelle est la taille de votre entreprise ?</h2>
-            <div class="mt-5">
-              <RadioCardList :inputs="radioInputsEntrepriseSize" />
-            </div>
-            <div class="h-max flex mt-2 justify-between w-11/12">
-              <a href="#entrepriseType">
-                <Button action="Précédent" />
-              </a>
-              <a href="#tools">
-                <Button action="Suivant" />
-              </a>
-            </div>
-
-          </div>
-        </section>
-        <section id="tools" class="mt-24 h-screen flex items-center">
-          <div>
-            <div class="w-max mb-10">
-              <h1 class="text-2xl font-semibold">Vos outils actuels</h1>
-              <div class="w-2/3 h-1 bg-primary-500 rounded mt-2"></div>
+        <Tabs :steps="['profile', 'dashboard', 'settings']">
+          <div class="hidden p-4 rounded-lg" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <div class="w-max mb-7">
+              <h1 class="text-2xl font-semibold">Informations de votre entreprise</h1>
+              <div class="w-1/3 h-1 bg-primary-500 rounded mt-2"></div>
             </div>
             <p class="w-5/6">
-              Les logiciels peuvent entraver la productivité industrielle. Transformer ces frustrations en solutions
-              fluides
-              est essentiel pour une performance optimale.
+              Personnalisez votre module industriel en toute simplicité avec notre formulaire dédié. Nous vous
+              accompagnons
+              pas
+              à
+              pas pour une solution sur mesure, adaptée à vos besoins.
             </p>
 
-            <div class="mt-10">
-              <h2 class="text-xl font-semibold">Quels types de logiciels utilisez-vous ?</h2>
+            <div class="mt-7">
+              <h2 class="text-xl font-semibold">Quels type d'entreprise êtes-vous ? (secteur d'activité)</h2>
               <div class="mt-5">
-                <CheckBoxCardList :inputs="inputsLogicielType" />
-              </div>
-              <div class="h-max flex mt-2 justify-between w-11/12">
-                <a href="#entrepriseSize">
-                  <Button action="Précédent" />
-                </a>
-                <a href="">
-                  <Button action="Suivant" />
-                </a>
+                <RadioCardList :inputs="inputsEntrepriseType" />
               </div>
             </div>
           </div>
-        </section>
+          <div class="hidden p-4 rounded-lg" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+            <div class="w-full">
+              <h2 class="text-xl font-semibold">Quelle est la taille de votre entreprise ?</h2>
+              <div class="mt-5">
+                <RadioCardList :inputs="radioInputsEntrepriseSize" />
+              </div>
+            </div>
+          </div>
+          <div class="hidden p-4 rounded-lg" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+            <div>
+              <div class="w-max mb-10">
+                <h1 class="text-2xl font-semibold">Vos outils actuels</h1>
+                <div class="w-2/3 h-1 bg-primary-500 rounded mt-2"></div>
+              </div>
+              <p class="w-5/6">
+                Les logiciels peuvent entraver la productivité industrielle. Transformer ces frustrations en solutions
+                fluides
+                est essentiel pour une performance optimale.
+              </p>
+
+              <div class="mt-10">
+                <h2 class="text-xl font-semibold">Quels types de logiciels utilisez-vous ?</h2>
+                <div class="mt-5">
+                  <CheckBoxCardList :inputs="inputsLogicielType" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Tabs>
       </div>
 
       <div class="w-1/2 flex items-center justify-center fixed right-0 top-20">
