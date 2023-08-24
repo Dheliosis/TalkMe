85c4f31db1dfe1727c3c0aa27f765584b8e977e1
Dhéliosis <fassot.caroline@gmail.com>
2023-08-24 14:41:22 +0200

    fourth question

--- app/app.vue
+++ app/app.vue
@@ -102,64 +102,210 @@ const radioInputsEntrepriseSize = [
     chiffre: "(+ 5 000 employés)"
   },
 ]
+
+const inputsPb = [
+  {
+    title: 'Manque de fonctionnalités'
+  }, {
+    title: 'Complexité d\'utilisation'
+  }, {
+    title: 'Problèmes de performance'
+  }, {
+    title: 'Conformité et Sécurité'
+  }, {
+    title: 'Support et service client'
+  }, {
+    title: 'Manque de personnalisation'
+  }, {
+    title: 'Accessibilité'
+  }, {
+    title: 'Incohérence des données'
+  }, {
+    title: 'Autres'
+  },
+]
+
+const inputsProcess = [
+  {
+    title: 'Gestion des stocks'
+  }, {
+    title: 'Planification de la production'
+  }, {
+    title: 'Contrôle qualité'
+  }, {
+    title: 'Gestion des commandes'
+  }, {
+    title: 'Maintenance préventive'
+  }, {
+    title: 'Rapports et analyses'
+  }, {
+    title: 'Gestion HR'
+  }, {
+    title: 'Suivi des expéditions'
+  }, {
+    title: 'Gestion des fournisseurs'
+  }, {
+    title: 'Sécurité et conformité'
+  }, {
+    title: 'Autres'
+  }
+]
+
+const inputsFonction = [
+  {
+    title: 'Tableaux de bord intuitifs'
+  }, {
+    title: 'Gestion des rémunérations'
+  }, {
+    title: 'Gestion des absences'
+  }, {
+    title: 'Gestion des congés'
+  }, {
+    title: 'Autres'
+  },
+]
+
+const inputsIso = [
+  {
+    title: 'ISO 1401'
+  }, {
+    title: 'ISO 9001'
+  }, {
+    title: 'Autres'
+  }, {
+    title: 'Non / Je ne sais pas'
+  },
+]
+
 </script>
 
 <template>
-  <Header progress1="10" progress2="100" progress3="89" :full="false"></Header>
-  <div class="mx-24 my-10 ">
+  <div class="mx-24 my-10 font-Poppins">
     <div class="flex w-full">
 
       <div class="w-1/2 h-screen scroll-smooth">
-        <Tabs :steps="['profile', 'dashboard', 'settings']">
-          <div class="hidden p-4 rounded-lg" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-            <div class="w-max mb-7">
-              <h1 class="text-2xl font-semibold">Informations de votre entreprise</h1>
-              <div class="w-1/3 h-1 bg-primary-500 rounded mt-2"></div>
+        <section id="entrepriseType">
+          <div class="w-max mb-7">
+            <h1 class="text-2xl font-semibold">Informations de votre entreprise</h1>
+            <div class="w-1/3 h-1 bg-primary-500 rounded mt-2"></div>
+          </div>
+          <p class="w-5/6">
+            Personnalisez votre module industriel en toute simplicité avec notre formulaire dédié. Nous vous accompagnons
+            pas
+            à
+            pas pour une solution sur mesure, adaptée à vos besoins.
+          </p>
+
+          <div class="mt-7">
+            <h2 class="text-xl font-semibold">Quels type d'entreprise êtes-vous&nbsp;? (secteur d'activité)</h2>
+            <div class="mt-5">
+              <RadioCardList :inputs="inputsEntrepriseType" />
+            </div>
+            <div class="h-max flex mt-4 justify-end w-11/12">
+              <a href="#entrepriseSize">
+                <Button action="Suivant" />
+              </a>
+            </div>
+          </div>
+
+        </section>
+        <section id="entrepriseSize" class="mt-24 h-screen flex items-center w-full">
+          <div class="w-full">
+            <h2 class="text-xl font-semibold">Quelle est la taille de votre entreprise&nbsp;?</h2>
+            <div class="mt-5">
+              <RadioCardList :inputs="radioInputsEntrepriseSize" />
+            </div>
+            <div class="h-max flex mt-4 justify-end w-11/12">
+              <a href="#tools">
+                <Button action="Suivant" />
+              </a>
+            </div>
+
+          </div>
+        </section>
+        <section id="tools" class="mt-24 h-screen flex items-center">
+          <div>
+            <div class="w-max mb-10">
+              <h1 class="text-2xl font-semibold">Vos outils actuels</h1>
+              <div class="w-2/3 h-1 bg-primary-500 rounded mt-2"></div>
             </div>
             <p class="w-5/6">
-              Personnalisez votre module industriel en toute simplicité avec notre formulaire dédié. Nous vous
-              accompagnons
-              pas
-              à
-              pas pour une solution sur mesure, adaptée à vos besoins.
+              Les logiciels peuvent entraver la productivité industrielle. Transformer ces frustrations en solutions
+              fluides
+              est essentiel pour une performance optimale.
             </p>
 
-            <div class="mt-7">
-              <h2 class="text-xl font-semibold">Quels type d'entreprise êtes-vous ? (secteur d'activité)</h2>
+            <div class="mt-10">
+              <h2 class="text-xl font-semibold">Quels types de logiciels utilisez-vous&nbsp;?</h2>
               <div class="mt-5">
-                <RadioCardList :inputs="inputsEntrepriseType" />
+                <CheckBoxCardList :inputs="inputsLogicielType" />
+              </div>
+              <div class="h-max flex mt-4 justify-end w-11/12">
+                <a href="#problems">
+                  <Button action="Suivant" />
+                </a>
               </div>
             </div>
           </div>
-          <div class="hidden p-4 rounded-lg" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
-            <div class="w-full">
-              <h2 class="text-xl font-semibold">Quelle est la taille de votre entreprise ?</h2>
-              <div class="mt-5">
-                <RadioCardList :inputs="radioInputsEntrepriseSize" />
-              </div>
+        </section>
+        <section id="problems" class="mt-24 h-screen flex items-center">
+          <div class="">
+            <h2 class="text-xl font-semibold">Quelles difficultés rencontrez-vous en utilisant vos logiciels
+              actuels&nbsp;?
+            </h2>
+            <div class="mt-5">
+              <CheckBoxCardList :inputs="inputsPb" />
+            </div>
+            <div class="h-max flex mt-4 justify-end w-11/12">
+              <a href="#process">
+                <Button action="Suivant" />
+              </a>
             </div>
           </div>
-          <div class="hidden p-4 rounded-lg" id="settings" role="tabpanel" aria-labelledby="settings-tab">
-            <div>
-              <div class="w-max mb-10">
-                <h1 class="text-2xl font-semibold">Vos outils actuels</h1>
-                <div class="w-2/3 h-1 bg-primary-500 rounded mt-2"></div>
-              </div>
-              <p class="w-5/6">
-                Les logiciels peuvent entraver la productivité industrielle. Transformer ces frustrations en solutions
-                fluides
-                est essentiel pour une performance optimale.
-              </p>
-
-              <div class="mt-10">
-                <h2 class="text-xl font-semibold">Quels types de logiciels utilisez-vous ?</h2>
-                <div class="mt-5">
-                  <CheckBoxCardList :inputs="inputsLogicielType" />
-                </div>
-              </div>
+        </section>
+        <section id="process" class="mt-24 h-screen flex items-center">
+          <div class="">
+            <h2 class="text-xl font-semibold">Quel processus souhaitez vous optimiser dans votre entreprise&nbsp;?
+            </h2>
+            <div class="mt-5">
+              <CheckBoxCardList :inputs="inputsProcess" />
+            </div>
+            <div class="h-max flex mt-4 justify-end w-11/12">
+              <a href="#function">
+                <Button action="Suivant" />
+              </a>
             </div>
           </div>
-        </Tabs>
+        </section>
+        <section id="function" class="mt-24 h-screen flex items-center">
+          <div class="">
+            <h2 class="text-xl font-semibold">Quelles fonctionnalités souhaitez-vous améliorer/implémenter dans vos
+              outils&nbsp;?
+            </h2>
+            <div class="mt-5">
+              <CheckBoxCardList :inputs="inputsFonction" />
+            </div>
+            <div class="h-max flex mt-4 justify-end w-11/12">
+              <a href="#iso">
+                <Button action="Suivant" />
+              </a>
+            </div>
+          </div>
+        </section>
+        <section id="iso" class="mt-24 h-screen flex items-center">
+          <div class="">
+            <h2 class="text-xl font-semibold">Respectez-vous des normes ISO&nbsp;?
+            </h2>
+            <div class="mt-5">
+              <CheckBoxCardList :inputs="inputsIso" />
+            </div>
+            <div class="h-max flex mt-4 justify-end w-11/12">
+              <a href="">
+                <Button action="Finir ma configuration" />
+              </a>
+            </div>
+          </div>
+        </section>
       </div>
 
       <div class="w-1/2 flex items-center justify-center fixed right-0 top-20">
